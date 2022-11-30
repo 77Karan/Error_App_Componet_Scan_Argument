00756a4f33a4f539ac99fed1e123003e54b4ba61
karankumar <kkarankumar890@gmail.com>
2022-11-30 20:46:15 +0530

    First Commit

---
+++ src/main/java/in/ashokit/Test.java
@@ -0,0 +1,14 @@
+package in.ashokit;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test 
+{
+	int n1=10;
+	Test(int n1)
+	{
+		System.out.println("creating the Test Obejct");
+	}
+
+}
